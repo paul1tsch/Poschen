bc9f688e33081d8880395a66805f47fab706383e
Dominik PAULITSCH <paulitsch@tuta.io>
2022-03-27 16:40:35 +0200

    v0.01

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+Color green = const Color(0x166534);
+Color lightgreen = const Color(0x22C55E);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text('Poschen'),
+          backgroundColor: lightgreen,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/background.png'),
+          ),
+        ),
       ),
     ),
   );
