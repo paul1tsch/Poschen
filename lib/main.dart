5220e0763b228811277348d6cba747d94dc8dc21
Dominik PAULITSCH <paulitsch@tuta.io>
2022-03-31 13:06:48 +0200

    Updating Menu and Bottom-Nav-bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:just_the_tooltip/just_the_tooltip.dart';
 
 Color green = const Color(0x166534);
 Color lightgreen = const Color(0x22C55E);
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+void _startGame() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          title: Text('Poschen'),
-          backgroundColor: lightgreen,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: '',
+              tooltip: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.question_mark),
+              label: '',
+              tooltip: 'About the Game',
+            )
+          ],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
         ),
-        body: Center(
+        body: SizedBox(
+          height: double.infinity,
           child: Image(
-            image: AssetImage('assets/background.png'),
+            image: AssetImage('assets/dice_background.png'),
+            fit: BoxFit.fitHeight,
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _startGame,
+          tooltip: 'Start Game',
+          child: const Icon(Icons.add),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
